d62aa4c7e09b65d6802b38a26b2bfb0b82fccc63
Sebastian Ramirez <85064772+sebas8822@users.noreply.github.com>
2022-11-19 17:33:36 +1100

    Practice enviroment functions and basics Kotlin

--- KotlinBasics/app/src/main/java/com/testsensor/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/testsensor/kotlinbasics/MainActivity.kt
@@ -12,7 +12,160 @@ class MainActivity : AppCompatActivity() {
         //useOfWhen2()
         //useOfWhen()
         //arrays()
-        collections()
+        //collections()
+        //loops()
+        //nullsafety()
+        //functions()
+        classes()
+
+    }
+
+    private fun classes() {
+        TODO("Not yet implemented")
+    }
+
+
+    /*Fuctions are blocks that are executed in a sequence when they are call
+    *
+    * */
+    private fun functions(){
+        sayHellow()
+        sayMyName("Sebas",)
+        sayMyNameAndAge("Sebas",34)
+        println(sumTwoNUmbers(30,80))
+    }
+
+    // Simple fuction
+    fun sayHellow(){
+        println("Hi there")
+    }
+    // Function with parameters
+    fun sayMyName(Name: String){
+        println("Hi there, $Name")
+    }
+
+    // Function with more htan 1 parameter
+    fun sayMyNameAndAge(Name: String,Age: Int){
+        println("Hi there, $Name your age is $Age")
+    }
+
+    // Function with a return
+    fun sumTwoNUmbers(FirtNum: Int,SecondNum: Int) : Int{
+        return FirtNum + SecondNum
+    }
+
+
+    /*
+    * Here we can see an example
+    * kotlin contain th function to indicate this error before to compile the program
+    * Kotlin avoid this errors before copile the program avoiding this happen when the program, is running
+    *
+    *
+    * */
+
+    private fun nullsafety() {
+        var myString = "Never stop to learn"
+        //myString  = null // this generate an error
+        println(myString)
+
+        //how we can create a variable that can take the null value and continuing running with any
+        //problem
+
+        var myString2: String? = "This can be transformed and take null values"
+
+        myString2 = null
+        println(myString2)
+        //this check is normarly in other programing languages
+//        if (myString2 != null) {
+//            println("$myString2")
+//        } else {
+//            println(myString2)
+//        }
+        // safety calls
+        //the ? it is a conditional which indicates
+        println(myString2?.length)
+        //this is another way to call a variable null without call the excepetion
+        myString2?.let {
+            println("$it")
+        }?: run{
+            println(myString2)
+        }
+
+
+    }
+
+
+    /*
+    Loops allows to go throught data structures like  arrays or maps
+    */
+    private fun loops() {
+        val myArray: List<String> =
+            listOf("Hi", "welcome to this training", "Please never stop to learn")
+        val myMaps: MutableMap<String, Int> =
+            mutableMapOf("Sebas" to 34, "Angela" to 31, "Karen" to 16)
+
+        //FOR
+
+        for (element in myArray) {
+            println(element)
+
+        }
+
+        for (element in myMaps) {
+            println("${element.key} = ${element.value}")
+        }
+
+        // use range
+        println(" with Range")
+        for (i in 0..10) {
+            println(i)
+        }
+        println(" with until")
+        for (i in 0 until 10) {// no count 10
+            println(i)
+        }
+
+        // with step
+
+        println(" with step")
+        for (i in 0..10 step 2) {
+            println(i)
+
+        }
+
+
+        // with decreasing
+
+        println(" with decreasing")
+        for (i in 10 downTo 0) {
+            println(i)
+        }
+
+        // with decreasing also with step
+
+        println(" with decreasing")
+        for (i in 10 downTo 0 step 2) {
+            println(i)
+        }
+
+
+        //trick
+        val myNumericArray = (0..100)
+        for (elements in myNumericArray) {
+            println(elements)
+        }
+
+
+        // Loop While
+
+        var x = 0
+
+        while (x <= 10) {
+            println(x)
+            x++
+
+        }
+
 
     }
 
@@ -30,30 +183,33 @@ class MainActivity : AppCompatActivity() {
     * */
     private fun collections() {
         // create a map or dicctionary
-        var myMap: Map<String,Int> = mapOf()
+        var myMap: Map<String, Int> = mapOf()
 
         println(myMap) // I'll show a empty map
 
         //Add elements to the map (This method creates a new map and delete the elements if they alredy exits)
-        myMap = mutableMapOf("Sebas" to 34, "Angela" to 31, "Karen" to 16)// mutable allows to add more varables na make changes
+        myMap = mutableMapOf(
+            "Sebas" to 34,
+            "Angela" to 31,
+            "Karen" to 16
+        )// mutable allows to add more varables na make changes
         println(myMap)
 
         //to add elemets to the exits maps
         myMap["Cata"] = 30 // shows a error because requires a mutable maps
-        myMap.put("Cata",30)
+        myMap.put("Cata", 30)
         println(myMap)
 
-        // Changes to Maps (call the key and =)
-        myMap.put("Cata",32)
+        // Updates or Changes to Maps (call the key and =)
+        myMap.put("Cata", 32)
         println(myMap)
 
         //data access
-        
+        println(myMap["Cata"])
 
-
-
-
-
+        //Delete Data
+        myMap.remove("Cata")
+        println(myMap)
 
 
     }
---
+++ KotlinBasics/app/src/main/java/com/testsensor/kotlinbasics/Programmer.kt
@@ -0,0 +1,4 @@
+package com.testsensor.kotlinbasics
+
+class Programmer {
+}
